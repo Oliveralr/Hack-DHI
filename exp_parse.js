9f024e986b0350f00d4ba92e571afb31917d62df
Adolfo Ramírez <37753765+Adolf97@users.noreply.github.com>
2019-03-02 07:54:44 -0600

    just one

--- exp_parse.js
+++ exp_parse.js
@@ -1,9 +1,8 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
-const url = 'http://dolores-hidalgo.com/lugares-tipo/entretenimiento/';
-const url2 = 'http://dolores-hidalgo.com/lugares-tipo/atractivos/';
+const url = 'http://dolores-hidalgo.com/lugares-tipo/atractivos/';
 
-/*entretenimiento parsing*/
+/*atractivo parsing*/
 const Parser = (url) => {
     return rp(url)
         .then((html) => {
@@ -18,19 +17,3 @@ const Parser = (url) => {
 }
 
 module.exports = Parser;
-
-/*atractivo parsing*/
-const Parser2 = (url2) => {
-    return rp(url2)
-        .then((html) => {
-            return {
-                title: $('.eltd-listing-title',html).text(),
-                location: $('.eltd-city',html).text()
-            }
-        })
-        .catch((err) => {
-            console.log('ERROR!');
-        })
-}
-
-module.exports = Parser2;
